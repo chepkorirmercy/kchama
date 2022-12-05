1de9fdbc49e86677c08d72979d2bed6dee6a72b1
Elvis Ndegwa Kangethe <63222738+Elvis888361@users.noreply.github.com>
2022-12-05 18:32:22 +0300

    working splashscreen

--- app/src/main/java/com/sharon/sample/mpesa/SplashScreen.java
+++ app/src/main/java/com/sharon/sample/mpesa/SplashScreen.java
@@ -29,7 +29,7 @@ public class SplashScreen extends AppCompatActivity {
             @Override
             public void run() {
                 Intent i=new Intent(SplashScreen.this,
-                        UserReg.class);
+                        Dashboard.class);
                 //Intent is used to switch from one activity to another.
 
                 startActivity(i);
--- app/src/main/java/com/sharon/sample/mpesa/UserDashboard.java
+++ app/src/main/java/com/sharon/sample/mpesa/UserDashboard.java
@@ -1,16 +1,23 @@
 package com.sharon.sample.mpesa;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class UserDashboard extends AppCompatActivity {
 
     private CardView pay, prog, meriGo;
-    private CardView select;
+    private CardView select, logout;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -20,11 +27,37 @@ public class UserDashboard extends AppCompatActivity {
         pay = findViewById(R.id.pay);
         select = findViewById(R.id.select);
         prog = findViewById(R.id.prog);
+        logout = findViewById(R.id.logout);
         meriGo = findViewById(R.id.M);
         pay.setOnClickListener(View -> {
             Intent intent = new Intent(UserDashboard.this, PaymentDetails.class);
             startActivity(intent);
 
+        });
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder= new AlertDialog.Builder(UserDashboard.this);
+                builder.setCancelable(false);
+                builder.setTitle("Exit");
+                builder.setMessage("are you sure you want to logout?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        FirebaseAuth.getInstance().signOut();
+                        //  delete data in shared preferences
+                        logout();
+                        startActivity(new Intent(UserDashboard.this,UserLogin.class));
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                builder.show();
+            }
         });
         prog.setOnClickListener(View -> {
             Intent intent = new Intent(UserDashboard.this, ProjectsActivity.class);
@@ -42,5 +75,10 @@ public class UserDashboard extends AppCompatActivity {
            startActivity(intent);
        });
 
+    }
+
+    private void logout() {
+        SharedPreferences settings = getSharedPreferences("user_details", Context.MODE_PRIVATE);
+        settings.edit().clear().commit();
     }
 }
