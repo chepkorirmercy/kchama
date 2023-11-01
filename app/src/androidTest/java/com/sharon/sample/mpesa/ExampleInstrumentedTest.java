e15e200f2da567edf2364c234b0d1942ad4648de
chepkorir mercy <chepkorirmercy413@gmail.com>
2023-11-01 13:23:23 +0300

    first commit

--- app/src/androidTest/java/com/sharon/sample/mpesa/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/sharon/sample/mpesa/ExampleInstrumentedTest.java
@@ -1,13 +1,16 @@
 package com.sharon.sample.mpesa;
 
+import static org.junit.Assert.assertEquals;
+
+import android.app.Instrumentation;
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+//import android.support.test.InstrumentationRegistry;
+//import android.support.test.runner.AndroidJUnit4;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,6 +19,8 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    private Instrumentation InstrumentationRegistry;
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
